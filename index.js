71e400723dd5492b79bafa8ea07d27bffdb11ba2
Harri Kapanen <harri.kapanen@iki.fi>
2019-01-09 23:18:52 +0200

    Topic scheme change, added a door switch

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ const idToName = {
   8: "ulkona",
   66: "vesimittari",
   164: "olohuone",
-  246: "varasto"
+  246: "varasto",
 }
 
 const ridToName = {
   43: "verstas",
   190: "aula"
+}
+
+const gidToName = {
+  11515: "etuovi"
 }
 
 startRtl_433()
@@ -36,14 +40,20 @@ function handleLine(line) {
 }
 
 function handleInputJson(json) {
-  if(json.model === 'THGR968' || json.model === 'THGR122N' || json.model === 'Nexus Temperature/Humidity') {
-    handleThgrOrNexus(json)
-  } else if(json.model === 'Prologue sensor') {
-    handlePrologue(json)
-  } else if(json.model === 'Waveman Switch Transmitter') {
-    handleSwitchTransmitter(json)
-  } else {
-    log.warn('Got unknown message', json)
+  switch (json.model) {
+    case 'THGR968':
+    case 'THGR122N':
+    case 'Nexus Temperature/Humidity':
+      handleThgrOrNexus(json)
+      break
+    case 'Prologue sensor':
+      handlePrologue(json)
+      break
+    case 'Generic Remote':
+      handleGenericRemote(json)
+      break
+    default:
+      log.warn('Got unknown message', json)
   }
 }
 
@@ -53,8 +63,8 @@ function handleThgrOrNexus(json) {
     log.warn('No instance mapping for rtl_433 ID', json.id)
     return
   }
-  mqttClient.publish(`/sensor/${instance}/temp`, JSON.stringify({ temperature: json.temperature_C, ts: new Date() }), { retain: true })
-  mqttClient.publish(`/sensor/${instance}/rhum`, JSON.stringify({ humidity: json.humidity, ts: new Date() }), { retain: true })
+  mqttClient.publish(`/sensor/${instance}`, JSON.stringify({ temperature: json.temperature_C, ts: new Date() }), { retain: true })
+  mqttClient.publish(`/sensor/${instance}`, JSON.stringify({ humidity: json.humidity, ts: new Date() }), { retain: true })
 }
 
 function handlePrologue(json) {
@@ -63,12 +73,20 @@ function handlePrologue(json) {
     log.warn('No instance mapping for rtl_433 ID', json.rid)
     return
   }
-  mqttClient.publish(`/sensor/${instance}/temp`, JSON.stringify({ temperature: json.temperature_C, ts: new Date() }), { retain: true })
-  mqttClient.publish(`/sensor/${instance}/rhum`, JSON.stringify({ humidity: json.humidity, ts: new Date() }), { retain: true })
+  mqttClient.publish(`/sensor/${instance}`, JSON.stringify({ temperature: json.temperature_C, ts: new Date() }), { retain: true })
+  mqttClient.publish(`/sensor/${instance}`, JSON.stringify({ humidity: json.humidity, ts: new Date() }), { retain: true })
 }
 
-function handleSwitchTransmitter(json) {
-  mqttClient.publish(`/switch/intertechno/${json.id.toLowerCase()}/${json.channel}/${json.button}`, json.state.toUpperCase(), { retain: true })
+function handleGenericRemote(json) {
+  const instance = gidToName[json.id]
+  if (json.cmd == '10') {
+    mqttClient.publish(`/sensor/${instance}`, JSON.stringify({ action: opened, ts: new Date() }), { retain: true })
+  } else if (json.cmd == '14') {
+    mqttClient.publish(`/sensor/${instance}`, JSON.stringify({ action: closed, ts: new Date() }), { retain: true })
+  } else {
+    log.warn('Unknown switch state', json.cmd)
+    return
+  }
 }
 
 function startMqttClient(brokerUrl) {
